0ff594700b828ca0855fa0869d2dbf2803496f47
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2025-10-26 18:30:36 +0100

    add start component and adjust routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { StartComponent } from './start/start.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: StartComponent,
+  },
+];
