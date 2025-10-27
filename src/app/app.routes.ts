fa9c2e35fca0bdd6b935c2ca1b015626cc0680fc
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2025-10-27 21:31:43 +0100

    add generator component and edit routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { StartComponent } from './start/start.component';
+import { GeneratorComponent } from './generator/generator.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: StartComponent,
+  },
+    {
+    path: 'generate_recipe',
+    component: GeneratorComponent,
   },
 ];
--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-start',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './start.component.html',
   styleUrl: './start.component.scss'
 })
