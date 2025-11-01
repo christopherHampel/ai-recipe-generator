0b139f153fd2ceb0f715e21311b0e051a36dbf55
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2025-11-01 09:17:09 +0100

    add dropdown recipe genertir

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../shared/header/header.component";
+import { HeaderComponent } from '../shared/header/header.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-generator',
   imports: [HeaderComponent, CommonModule],
   templateUrl: './generator.component.html',
-  styleUrl: './generator.component.scss'
+  styleUrl: './generator.component.scss',
 })
 export class GeneratorComponent {
-  
+  isOpen = false;
+  units = ['grams', 'ml', 'piece'];
+  selectedUnit = 'grams';
+
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
+
+  selectUnit(unit: string) {
+    this.selectedUnit = unit;
+    this.isOpen = false;
+  }
+
+  closeDropdown() {
+    this.isOpen = false;
+  }
 }
