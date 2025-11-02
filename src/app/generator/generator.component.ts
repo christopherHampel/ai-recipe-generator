703ae924d54ff13e2a2703853ba9bfa3d0bb2efa
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2025-11-02 10:55:53 +0100

    add ingredients logic edit

--- src/app/generator/generator.component.ts
+++ src/app/generator/generator.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-generator',
-  imports: [HeaderComponent, CommonModule],
+  imports: [HeaderComponent, CommonModule, FormsModule],
   templateUrl: './generator.component.html',
   styleUrl: './generator.component.scss',
 })
@@ -12,6 +13,34 @@ export class GeneratorComponent {
   isOpen = false;
   units = ['grams', 'ml', 'piece'];
   selectedUnit = 'grams';
+
+  ingredient: string = '';
+  crowd: number | null = null;
+  addedIngredients: { name: string; crowd: number; unit: string }[] = [];
+
+  // addedIngredients: any = [
+  //   {
+  //     name: 'Zwiebel',
+  //     crowd: '2',
+  //     unit: 'piece',
+  //   },
+  //   {
+  //     name: 'Mehl',
+  //     crowd: '200',
+  //     unit: 'grams',
+  //   },
+  //   {
+  //     name: 'Milch',
+  //     crowd: '100',
+  //     unit: 'ml',
+  //   },
+
+  //   {
+  //     name: 'Milch',
+  //     crowd: '100',
+  //     unit: 'ml',
+  //   },
+  // ];
 
   toggleDropdown() {
     this.isOpen = !this.isOpen;
@@ -24,5 +53,23 @@ export class GeneratorComponent {
 
   closeDropdown() {
     this.isOpen = false;
+  }
+
+  addIngredient() {
+    if (!this.ingredient || this.crowd === null) return;
+
+    const newIngredient = {
+      name: this.ingredient,
+      crowd: this.crowd,
+      unit: this.selectedUnit,
+    };
+
+    this.addedIngredients.push(newIngredient);
+
+    this.ingredient = '';
+    this.crowd = null;
+
+    console.log(this.addedIngredients);
+    
   }
 }
