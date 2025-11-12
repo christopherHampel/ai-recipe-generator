6f490d4e70375729430005b07e2ae296f675efb3
christopherHampel <161190884+christopherHampel@users.noreply.github.com>
2025-11-12 22:22:00 +0100

    edit diet preferences at preferences-generator

--- src/app/generator/preferences/diet-preferences/diet-preferences.component.ts
+++ src/app/generator/preferences/diet-preferences/diet-preferences.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IngredientService } from '../../../services/ingredient.service';
 
 @Component({
   selector: 'app-diet-preferences',
@@ -7,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './diet-preferences.component.scss'
 })
 export class DietPreferencesComponent {
+private ingredientService = inject(IngredientService);
+
+dietPreferences: string[] = [
+  'Vegetarian',
+  'Vegan',
+  'Keto',
+  'No preferences',
+];
+
+  selectCuisineStyle(cuisine: string) {
+    if (!this.styleIncludes(cuisine)) {
+      this.ingredientService.selectedCuisineStyle.push(cuisine);
+    } else {
+      this.removeCuisineStyle(cuisine);
+    }
+    console.log(this.ingredientService.selectedCuisineStyle);
+  }
+
+  styleIncludes(cuisine:string) {
+    return false
+  }
+
+  removeCuisineStyle(cuisine:string) {}
 
 }
